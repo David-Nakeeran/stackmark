60ddcd5f2fd79cafafbb99d9cef7a9329d67087e
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2026-04-27 16:48:00 +0100

    Update: add validation and relationships

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,3 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :user
+  has_many :bookmark_tags, dependent: :destroy
+  has_many :tags, through: :bookmark_tags
+  validates :url, presence: true
+  validates :title, presence: true
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,5 @@
 class Tag < ApplicationRecord
+  has_many :bookmark_tags, dependent: :destroy
+  has_many :bookmarks, through: :bookmark_tags
+  validates :name, presence: true, uniqueness: true
 end
